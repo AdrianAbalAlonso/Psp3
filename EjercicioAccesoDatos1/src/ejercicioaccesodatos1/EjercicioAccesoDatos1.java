4b744c21008d576adfa7d58e707a76a9a4e0f2fd
Adrián <Adrin@192.168.0.19>
2016-09-27 14:36:42 +0200

    Segundo commit, probando a comprobar is la ruta existe

--- EjercicioAccesoDatos1/src/ejercicioaccesodatos1/EjercicioAccesoDatos1.java
+++ EjercicioAccesoDatos1/src/ejercicioaccesodatos1/EjercicioAccesoDatos1.java
@@ -4,7 +4,12 @@
  * and open the template in the editor.
  */
 package ejercicioaccesodatos1;
-
+import java.io.File;
+import java.io.FilenameFilter;
+import java.io.IOException;
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
 /**
  *
  * @author Adrián
@@ -14,8 +19,33 @@ public class EjercicioAccesoDatos1 {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
+    public static void main(String[] args) throws IOException {
+       // Crear o directorio 'arquivosdir' na ruta '/home/oracle/NetBeansProjects/arquivos/' sempre e cando dito directorio non exista
+       String rutaArquivosdir = "/home/oracle/NetBeansProjects/arquivos/arquivosdir/";
+       File arquivosdir = new File(rutaArquivosdir);
+        arquivosdir.mkdirs();
+       //Comprobar que a ruta foi creada ou existe mediante o comandos do sistema operativo
+       
+       //crear  o arquivo Products1.txt no directorio mencionado anteriormente (arquivosdir) sempre e cando dito arquivo non exista. 
+         arquivosdir = new File(rutaArquivosdir + "Products1.txt");
+        arquivosdir.createNewFile();
+       //comprobar que o arquivo foi creado ou existe mediante comandos do sistema operativo        
+            
+        
+       //comprobar que a ruta foi creada mediante o metodo da clase File axeitado .En este caso con un if , igualado a true es suficiente.
+       rutaArquivosdir.
+       //comprobar que o arquivo foi creado mediante o metodo da clase File axeitado
+           if (arquivosdir.exists() == true) {
+            System.out.println("\n Si existe el archivo Products1");
+        } else {
+            System.out.println("\n No existe el archivo");
+        }         
+       
+
+        
+        
+        
+        
     }
     
 }
